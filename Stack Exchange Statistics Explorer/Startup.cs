9706744eb3f99615b101a96d9f80b97d95e57ae4
EBrown8534 <ebrown8534@gmail.com>
2016-08-19 04:05:05 -0400

    Microsoft needs to fix the formatting in `Startup.cs` so I don't have to. >.>

--- Stack Exchange Statistics Explorer/Startup.cs
+++ Stack Exchange Statistics Explorer/Startup.cs
@@ -4,8 +4,10 @@ using Owin;
 [assembly: OwinStartupAttribute(typeof(Stack_Exchange_Statistics_Explorer.Startup))]
 namespace Stack_Exchange_Statistics_Explorer
 {
-    public partial class Startup {
-        public void Configuration(IAppBuilder app) {
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
             ConfigureAuth(app);
         }
     }
