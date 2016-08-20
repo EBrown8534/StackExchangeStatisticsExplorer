8799f6f33bf5df8c5745ef5f9d2a401bcbff70a0
EBrown8534 <ebrown8534@gmail.com>
2016-08-20 01:58:48 -0400

    Added most of the information needed for `SystemHealth.aspx` for the moment.

--- Stack Exchange Statistics Explorer/SystemHealth.aspx
+++ Stack Exchange Statistics Explorer/SystemHealth.aspx
@@ -1,3 +1,60 @@
-﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="SystemHealth.aspx.cs" Inherits="Stack_Exchange_Statistics_Explorer.SystemHealth" %>
+﻿<%@ Page Title="System Health" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="SystemHealth.aspx.cs" Inherits="Stack_Exchange_Statistics_Explorer.SystemHealth" %>
+
+<%@ Import Namespace="Stack_Exchange_Statistics_Explorer.Utilities" %>
+<%@ Import Namespace="Stack_Exchange_Statistics_Explorer.Utilities.Extensions" %>
+<%@ Import Namespace="Evbpc.Framework.Utilities.Extensions" %>
+
 <asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+    <h2>System Health</h2>
+    <h3>Data Gathering Health</h3>
+    <asp:ListView runat="server" ID="ApiLogResults">
+        <LayoutTemplate>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Start</th>
+                        <th>End</th>
+                        <th>Time Taken</th>
+                        <th>Total Requests</th>
+                        <th>Requests / Second</th>
+                        <th>Sites Loaded</th>
+                        <th>Milliseconds / Site</th>
+                        <th>Backoff Count</th>
+                        <th>Backoff Seconds</th>
+                        <th>End Quota</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr runat="server" id="groupPlaceholder"></tr>
+                </tbody>
+            </table>
+        </LayoutTemplate>
+
+        <GroupTemplate>
+            <tr runat="server" id="itemPlaceholder"></tr>
+        </GroupTemplate>
+
+        <ItemTemplate>
+            <tr runat="server">
+                <td><%#((ApiBatchLog)Container.DataItem).StartDateTime.ToString("yyyy-MM-dd HH:mm:ss.fffffff") %></td>
+                <td><%#((ApiBatchLog)Container.DataItem).EndDateTime.ToString("yyyy-MM-dd HH:mm:ss.fffffff") %></td>
+                <td>
+                    <%#((ApiBatchLog)Container.DataItem).TimeTaken %>
+                </td>
+                <td><%#((ApiBatchLog)Container.DataItem).RequestCount %></td>
+                <td class='<%#new Dictionary<string, Predicate<double>> { ["good"] = x => x >= 3, ["neutral"] = x => x > 2, ["bad"] = x => true }.FindKey(((ApiBatchLog)Container.DataItem).RequestsPerSecond) %>'>
+                    <%#((ApiBatchLog)Container.DataItem).RequestsPerSecond.ToString("0.00") %>
+                </td>
+                <td><%#((ApiBatchLog)Container.DataItem).SiteCount %></td>
+                <td class='<%#new Dictionary<string, Predicate<double>> { ["good"] = x => x <= 1200, ["neutral"] = x => x <= 1500, ["bad"] = x => true }.FindKey(((ApiBatchLog)Container.DataItem).MillisecondsPerSite) %>'>
+                    <%#((ApiBatchLog)Container.DataItem).MillisecondsPerSite.ToString("0.00") %>
+                </td>
+                <td><%#((ApiBatchLog)Container.DataItem).BackoffCount %></td>
+                <td class='<%#new Dictionary<string, Predicate<int>> { ["good"] = x => x <= 90, ["neutral"] = x => x <= 150, ["bad"] = x => true }.FindKey(((ApiBatchLog)Container.DataItem).TotalBackoff) %>'>
+                    <%#((ApiBatchLog)Container.DataItem).TotalBackoff %>
+                </td>
+                <td><%#((ApiBatchLog)Container.DataItem).EndQuotaRemaining %></td>
+            </tr>
+        </ItemTemplate>
+    </asp:ListView>
 </asp:Content>
--- Stack Exchange Statistics Explorer/SystemHealth.aspx.cs
+++ Stack Exchange Statistics Explorer/SystemHealth.aspx.cs
@@ -1,5 +1,9 @@
-﻿using System;
+﻿using Stack_Exchange_Statistics_Explorer.Utilities.Extensions;
+using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -11,7 +15,102 @@ namespace Stack_Exchange_Statistics_Explorer
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            var logResults = new List<ApiBatchLog>();
 
+            using (var connection = new SqlConnection(ConfigurationManager.ConnectionStrings["ApiDataConnection"].ConnectionString))
+            {
+                if (connection.State != ConnectionState.Open)
+                {
+                    connection.Open();
+                }
+
+                logResults = ApiBatchLog.LoadAllFromDatabase(connection);
+            }
+
+            var apiLogResults = new List<ApiBatchLog>();
+
+            foreach (var logResult in logResults.OrderBy(x => x.StartDateTime))
+            {
+                if (apiLogResults.Where(x => x.StartDateTime.Date == logResult.StartDateTime.Date).Count() == 0)
+                {
+                    apiLogResults.Add(logResult);
+                }
+            }
+
+            apiLogResults = apiLogResults.OrderByDescending(x => x.StartDateTime).ToList();
+
+            ApiLogResults.DataSource = apiLogResults;
+            ApiLogResults.DataBind();
+        }
+
+        protected class ApiBatchLog
+        {
+            public Guid Id { get; set; }
+            public DateTime StartDateTime { get; set; }
+            public DateTime EndDateTime { get; set; }
+            public DateTime AddedDateTime { get; set; }
+            public string RequestedBy { get; set; }
+            public int RequestCount { get; set; }
+            public int BackoffCount { get; set; }
+            public int TotalBackoff { get; set; }
+            public int HasMoreCount { get; set; }
+            public int QuotaMax { get; set; }
+            public int StartQuotaRemaining { get; set; }
+            public int EndQuotaRemaining { get; set; }
+            public int SiteCount { get; set; }
+
+            public double RequestsPerSecond => RequestCount / (EndDateTime - StartDateTime).TotalSeconds;
+            public TimeSpan TimeTaken => EndDateTime - StartDateTime;
+            public double MillisecondsPerSite => (EndDateTime - StartDateTime).TotalMilliseconds / SiteCount;
+
+            public static ApiBatchLog LoadFromReader(SqlDataReader reader) => new ApiBatchLog
+            {
+                Id = reader.GetItem<Guid>(nameof(Id)),
+                StartDateTime = reader.GetItem<DateTime>(nameof(StartDateTime)),
+                EndDateTime = reader.GetItem<DateTime>(nameof(EndDateTime)),
+                AddedDateTime = reader.GetItem<DateTime>(nameof(AddedDateTime)),
+                RequestedBy = reader.GetItem<string>(nameof(RequestedBy)),
+                RequestCount = reader.GetItem<int>(nameof(RequestCount)),
+                BackoffCount = reader.GetItem<int>(nameof(BackoffCount)),
+                TotalBackoff = reader.GetItem<int>(nameof(TotalBackoff)),
+                HasMoreCount = reader.GetItem<int>(nameof(HasMoreCount)),
+                QuotaMax = reader.GetItem<int>(nameof(QuotaMax)),
+                StartQuotaRemaining = reader.GetItem<int>(nameof(StartQuotaRemaining)),
+                EndQuotaRemaining = reader.GetItem<int>(nameof(EndQuotaRemaining)),
+                SiteCount = reader.GetItem<int>(nameof(SiteCount))
+            };
+
+            public static List<ApiBatchLog> LoadAllFromDatabase(SqlConnection connection)
+            {
+                var results = new List<ApiBatchLog>();
+
+                using (var command = new SqlCommand(@"SELECT
+    b.*
+    ,[ItemCount] AS[SiteCount]
+FROM
+    SE.ApiBatchLog b
+INNER JOIN
+    SE.ApiLog
+ON
+    Id = BatchId
+    AND
+    StartDateTime = RequestDateTime
+WHERE
+    b.RequestedBy <> 'BADBATCH'
+    AND
+    b.RequestedBy <> 'INCOMPLETEBATCH'
+ORDER BY
+    EndDateTime DESC", connection))
+                using (var reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        results.Add(LoadFromReader(reader));
+                    }
+                }
+
+                return results;
+            }
         }
     }
 }
